8493094e120901ac2ded45545a355cd5c859c379
Peter Lyons <pete@peterlyons.com>
2022-01-18 00:06:17 +0000

    bastardkb/tbkmini/keymaps/pl-2106 compiling

---
+++ keyboards/bastardkb/tbkmini/keymaps/pl-2106/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// #define DIODE_DIRECTION COL2ROW
+
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
+#define PERMISSIVE_HOLD
+#define TAPPING_TERM 300
+
+#define COMBO_COUNT 4
+
+#define NO_AUTO_SHIFT_ALPHA
