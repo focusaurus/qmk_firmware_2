f33da4f1c7df6a50c9473170d26efcd9d9346d0a
Peter Lyons <pete@peterlyons.com>
2022-01-18 00:14:33 +0000

    Got tbkmini firmware small enough for pl-2106 keymap

--- keyboards/bastardkb/tbkmini/rules.mk
+++ keyboards/bastardkb/tbkmini/rules.mk
@@ -15,7 +15,6 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 RGB_MATRIX_ENABLE = yes     # Enable keyboard RGB matrix (do not use together with RGBLIGHT_ENABLE)
 RGB_MATRIX_DRIVER = WS2812  # RGB matrix driver support
 AUDIO_ENABLE = no           # Audio output
