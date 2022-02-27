c8cd37ee5165cac4476710db2b6be9171804412e
Peter Lyons <pete@peterlyons.com>
2022-02-27 00:55:50 +0000

    forgot bottom row mods screws with chording

--- keyboards/squeezebox/v2112/keymaps/focusaurus/keymap.c
+++ keyboards/squeezebox/v2112/keymaps/focusaurus/keymap.c
@@ -101,16 +101,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // TODO 1-finger combos for uk, ej, oq
 // TODO 1-finger combo for semicolon
 // left hand
-KC_TAB, KC_QUOTE, TD(TD_COMMA), KC_DOT, KC_P, KC_Y,
+KC_TAB, KC_QUOTE, TD(TD_COMMA), LALT_T(KC_DOT), LCTL_T(KC_P), KC_Y,
 MT(MOD_LCTL, KC_ESC), MT(MOD_LGUI | MOD_LALT, KC_A), KC_O, KC_E, KC_U, KC_I,
-KC_SCLN, KC_COLON /*TD(TD_SEMI)*/, KC_Q, LALT_T(KC_J), LCTL_T(KC_K), KC_X,
+KC_SCLN, KC_COLON /*TD(TD_SEMI)*/, KC_Q, KC_J, KC_K, KC_X,
 LT(KBFN, KC_BSPC), LT(NAVNUM, LEADER), OSM(MOD_LSFT),
 
 // TODO 1-finger combos for hm, tw, nv, sz
 // right hand
-KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH,
+KC_F, RCTL_T(KC_G), KC_C, KC_R, KC_L, KC_SLSH,
 KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINUS,
-KC_B, RCTL_T(KC_M), LALT_T(KC_W), KC_V, KC_Z, OSM(MOD_LALT),
+KC_B, KC_M, LALT_T(KC_W), KC_V, KC_Z, OSM(MOD_LALT),
 KC_ENT, LT(BANG, KC_SPC), OSM(MOD_RCTL)
 
 ), [MACOS] = LAYOUT(
