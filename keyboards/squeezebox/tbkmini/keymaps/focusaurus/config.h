edeee58de4f637263a348e72003869440d7dc68c
Peter Lyons <pete@peterlyons.com>
2022-03-25 11:41:54 +0000

    no mod-tap on nav down key because it breaks hold to repeat

--- keyboards/squeezebox/tbkmini/keymaps/focusaurus/config.h
+++ keyboards/squeezebox/tbkmini/keymaps/focusaurus/config.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define COMBO_COUNT 4
+#define COMBO_COUNT 6
 #define COMBO_TERM 150
-// #define TAPPING_TERM 300
+#define TAPPING_TERM 300
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
 #define PERMISSIVE_HOLD
