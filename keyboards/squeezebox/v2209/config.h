7160bfea6b3fa30d2b09caa6737b5c095b7ae60f
Peter Lyons <pete@peterlyons.com>
2022-09-28 13:09:06 -0400

    tweak for PCB side mistake

--- keyboards/squeezebox/v2209/config.h
+++ keyboards/squeezebox/v2209/config.h
@@ -27,9 +27,12 @@
  */
 // Left side is B5, B4, E6, D7, C6, D4, D0, D1, GND, GND, D2, D3
 // right side is B6, B2, B3, B1, F7, F6, F5, F4, VCC, RST, GND, B0
-// For squeezebox v2209
+// For squeezebox v2209 (PCB side A)
 #define MATRIX_ROW_PINS { B5, B4, E6, D7 }
 #define MATRIX_COL_PINS { F5, F6, F7, B1, B3 }
+// For squeezebox v2209 (PCB side B)
+// #define MATRIX_ROW_PINS { B1, B3, B2, B6 }
+// #define MATRIX_COL_PINS { D0, D4, C6, D7, E6 }
 // For bastardkb/tbkmini
 // #define MATRIX_ROW_PINS { B6, B2, B3 }
 // #define MATRIX_COL_PINS { B5, B4, E6, D7, C6, D4, D0 }
